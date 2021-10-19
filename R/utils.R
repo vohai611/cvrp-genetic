36535f32c14ed8650badb583a933908ada4759ee
Haivo <hai835559@gmail.com>
2021-10-19 15:18:24 +0200

    move visualzie helper

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,7 @@
-# read vrp files
+
+# Read benchmark data -----------------------------------------------------------------------------------
+
+
 read_vrp_bench = function(path) {
   
   # metadata
@@ -43,5 +46,36 @@ read_vrp_bench = function(path) {
   
 }
 
+
+# Visualize path ----------------------------------------------------------------------------------------
+
+vrp_map = function(file, result) {
+  fitness_val = result$distance
+  rs_path = result$path
+  
+  rs_path = rs_path %>% embed(2) %>% 
+    as_tibble(.name_repair = ~c("from", "to")) %>% 
+    mutate(group = cumsum(to == 1))
+  
+  my_col = paletteer::paletteer_d("ggsci::category20b_d3")
+  
+  
+  file$file %>% left_join(rs_path, by = c("node" = "from")) %>% 
+    left_join(file$file, suffix = c("_from", "_to"), by= c('to' = 'node')) %>% 
+    ggplot(aes(x = x_from, y_from, color = as_factor(group) ))+
+    geom_point()+
+    geom_curve(aes(xend = x_to, yend =y_to), 
+               arrow = arrow(angle = 10, type = "closed"), 
+               curvature = 0,
+               alpha = .7, show.legend = F)+
+    geom_label(aes(label = node), position = position_nudge(x= 1, y = 1),
+               show.legend = F)+
+    labs(title = paste0("Result of data set: ", file$metadata[1]),
+         subtitle = paste0("Total distance: ",fitness_val, "\n",
+                           file$metadata[3]),
+         color= "Sub-tour") +
+    scale_color_manual(values = my_col) +
+    theme_void()
+}
 
   
